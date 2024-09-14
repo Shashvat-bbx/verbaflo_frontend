ea8451c67de3eed8340e9faa534d05a85adc385b
unknown <shashvat990@gmail.com>
2024-09-15 00:27:22 +0530

    changed spelling

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Accordion from '@/components/ui/accordion';
 export default function Main() {
   return (
     <div className="flex flex-col items-center m-5 p-5">
-      <div className="text-3xl font-bold">Resume Builder</div>
+      <div className="text-3xl font-bold">Resume Biulder</div>
       <Upload />
       <Accordion>
       </Accordion>
